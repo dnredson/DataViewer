1ce5683dcdf92e52ca2ac53427dafbd5126a0aa0
Fabrizio Borelli <ffborelli@gmail.com>
2020-05-05 16:49:30 -0300

    feat : getNodes Promises Race

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* importar as configurações do servidor */
 var app = require("./config/server");
 
+
+
 /* parametrizar a porta de escuta */
 app.listen(80, function() {
   console.log("Servidor online");
